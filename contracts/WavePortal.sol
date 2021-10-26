8183a9a335b29681d735f43259ee6c3dc5667bd9
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2021-10-25 23:32:16 -0400

    add functionality to wave & track wave count

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.0; // version of Solidity we want our program to use
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // State Variables
+    uint256 totalWaves; // Automatically initialized to 0 & permanently stored in contract storage.
+
     constructor() {
         console.log("Wave Portal activated.");
     }
-}
+
+    function wave() public {
+        totalWaves+=1;
+        console.log("%s waved", msg.sender); // msg.sender is the wallet address that invoked the function.
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We've received %d total waves", totalWaves);
+        return totalWaves;
+    }
+} 
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,27 @@
 // Deploy smart contract to network
 const main = async () => {
     // hre is Hardhat Runtime Env, passed in with `npx hardhat` command
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed(); // wait for contract to be deployed
-    console.log(`Deployed to: ${waveContract.address}`);
+    console.log(`Deployed to ${waveContract.address}, by ${owner.address}`);
+
+
+    // Running as owner.
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave(); // Invoke wave function.
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    // Running as other person.
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }
 
 const runMain = async () => {
